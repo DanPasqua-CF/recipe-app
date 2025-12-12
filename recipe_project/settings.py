d9cef2adc36d6f5951d0684cf34b1d0504483caf
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-12-12 15:37:20 -0500

    Update ALLOWED_HOSTS with site and development server URL

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    "",
-    "",
+    "https://cryptic-falls-05698-5c33a6719738.herokuapp.com/",
     "localhost",
     "127.0.0.1",
 ]
