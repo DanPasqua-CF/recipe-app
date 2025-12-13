997d5254fd639562e1153cbeeffdd3f7174595de
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-12-12 21:53:17 -0500

    Fix settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -123,8 +123,6 @@ WHITENOISE_AUTOREFRESH = True
 # -----------------------------
 # AWS S3 CONFIGURATION
 # -----------------------------
-INSTALLED_APPS += ['storages']
-
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
