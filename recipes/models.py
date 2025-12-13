0e4760c88f84bb1b5770819a1da8646c3cf271a0
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-12-12 19:07:28 -0500

    Update stuff

--- recipes/models.py
+++ recipes/models.py
@@ -2,14 +2,25 @@ from django.db import models
 from django.urls import reverse
 
 class Recipe(models.Model):
+    # -----------------------------
+    # Fields
+    # -----------------------------
     name = models.CharField(max_length=50)
     ingredients = models.CharField(
         max_length=225,
         help_text="Enter the ingredients, separated by a comma"
     )
     cooking_time = models.IntegerField(help_text="Cooking time in minutes")
-    pic = models.ImageField(upload_to='', default='no_picture.jpg')  # AWS_LOCATION handles folder
+    pic = models.ImageField(
+        upload_to='recipes/',  # saves files under 'recipes/' in S3
+        default='recipes/no_picture.jpg',
+        blank=True,
+        null=True
+    )
 
+    # -----------------------------
+    # Calculate difficulty
+    # -----------------------------
     @property
     def calculate_difficulty(self):
         ingredients_list = self.ingredients.split(", ")
@@ -19,12 +30,17 @@ class Recipe(models.Model):
             return "Medium"
         elif self.cooking_time >= 10 and len(ingredients_list) < 4:
             return "Intermediate"
-        elif self.cooking_time >= 10 and len(ingredients_list) >= 4:
+        else:
             return "Hard"
-        return None
 
+    # -----------------------------
+    # String representation
+    # -----------------------------
     def __str__(self):
-        return str(self.name)
+        return self.name
 
+    # -----------------------------
+    # Absolute URL
+    # -----------------------------
     def get_absolute_url(self):
         return reverse("recipes:detail", kwargs={"pk": self.pk})
